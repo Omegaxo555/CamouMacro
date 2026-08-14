95fdeb572157337471ef0822afc2d9f55add16d8
OmegaxoDEV <paulo.ruiz555@hotmail.com>
2026-08-14 04:23:29 -0400

    xddd

--- modules/allfeellove_auto.py
+++ modules/allfeellove_auto.py
@@ -44,7 +44,7 @@ class AllfeelloveAuto:
         gender_button = HtmlElement.css("label.gender__item.male")
         lookingfor_button = HtmlElement.css('label.checkbox__item.woman.woman_female')
         name_input = HtmlElement.css('input.input.input_name.name-container__input')
-        date_input = HtmlElement.css('input[data-testid="date-input"]')
+        date_input = HtmlElement.xpath('/html/body/div[1]/div/div/main/div[1]/div/div[2]/div/div/form/div[3]/div[2]/div[1]/input')
         terms_checkbox = HtmlElement.css("input.terms__label")
 
         for label, selector in {
